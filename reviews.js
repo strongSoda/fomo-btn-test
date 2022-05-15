410d2601e53ca5822d8dde08c9df556e38636cb1
IMRAN KHAN <30754328+strongSoda@users.noreply.github.com>
2022-05-15 23:25:36 +0530

    Create reviews.js

---
+++ reviews.js
@@ -0,0 +1,79 @@
+const REVIEWS = [
+  {
+    id: 0,
+    author: "Jack F.",
+    text: "Fantastic, I'm totally blown away by Testimonial Generator.",
+    img:
+      "https://cdn-cmhkh.nitrocdn.com/TezMmPgFmiYDBaHMYdPwMCWAKAYkIeiI/assets/static/optimized/www.theloremipsumco.com/wp-content/uploads/2014/04/73e4a3fe6ad959478d8562505bf1e43e.pic7_3000x2003-e1482875351271.jpg",
+    lat: "18.180555",
+    lng: "-66.749961",
+    zip: "00601"
+  },
+  {
+    id: 1,
+    author: "Kelsi Gordon",
+    text:
+      "My busy schedule leaves little, if any, time for blogging and social media. The Lorem Ipsum Company has been a huge part of helping me grow my business through organic search and content marketing",
+    img:
+      "https://cdn-cmhkh.nitrocdn.com/TezMmPgFmiYDBaHMYdPwMCWAKAYkIeiI/assets/static/optimized/www.theloremipsumco.com/wp-content/uploads/2014/04/16cf23915a4dc235a62f204704f4b424.Gordon-Kelsi.jpg",
+    lat: "18.361945",
+    lng: "-67.175597",
+    zip: "00602"
+  },
+  {
+    id: 2,
+    author: "Seth Gewirtz",
+    text:
+      "Jeramy and his team at the Lorem Ipsum Company whipped my website into shape just in time for tax season. I was excited by the results and am proud to direct clients to my website once again.",
+    img:
+      "https://cdn-cmhkh.nitrocdn.com/TezMmPgFmiYDBaHMYdPwMCWAKAYkIeiI/assets/static/optimized/www.theloremipsumco.com/wp-content/uploads/2017/01/5f92099ec3f04b259e1a4a80f5b35d6b.KellySwedaPhotography.Seth_Gewirtz_high_res-7628-e1483741109998.jpg",
+    lat: "18.455183",
+    lng: "-67.11988",
+    zip: "00603"
+  },
+  {
+    id: 3,
+    author: "Tasha Zuzalek",
+    text:
+      "Professional, responsive, and able to keep up with ever-changing demand and tight deadlines: That's how I would describe Jeramy and his team at The Lorem Ipsum Company.",
+    img:
+      "https://cdn-cmhkh.nitrocdn.com/TezMmPgFmiYDBaHMYdPwMCWAKAYkIeiI/assets/static/optimized/www.theloremipsumco.com/wp-content/uploads/2018/01/b982a32fcc131dd975ccbd3b233b54c2.374c71c.jpg",
+    lat: "18.158345",
+    lng: "-66.932911",
+    zip: "00606"
+  },
+  {
+    id: 4,
+    author: "Rob Joor",
+    text:
+      "After being forced to move twice within five years, our customers had a hard time finding us and our sales plummeted.",
+    img:
+      "https://cdn-cmhkh.nitrocdn.com/TezMmPgFmiYDBaHMYdPwMCWAKAYkIeiI/assets/static/optimized/www.theloremipsumco.com/wp-content/uploads/0a4e069d3c2609fa474c91abfffa6ad5.RobJoor.jpg",
+    lat: "18.295366",
+    lng: "-67.125135",
+    zip: "00610"
+  },
+  {
+    id: 5,
+    author: "Mike Brashears",
+    text:
+      "I was skeptical of SEO and content marketing at first, but the Lorem Ipsum Company not only proved itself financially speaking, but the response I have received from customers is incredible.",
+    img:
+      "https://cdn-cmhkh.nitrocdn.com/TezMmPgFmiYDBaHMYdPwMCWAKAYkIeiI/assets/static/optimized/www.theloremipsumco.com/wp-content/uploads/3f0c076943d0ab6573abd32bccc79b95.0-3.jpg",
+    lat: "18.402253",
+    lng: "-66.711397",
+    zip: "00612"
+  },
+  {
+    id: 6,
+    author: "Rick E.",
+    text:
+      "This is unbelievable. After using Testiminial Generator my buisness skyrocketed!",
+    img: "https://www.tecocraft.com/wp-content/uploads/2018/04/1.jpg",
+    lat: "18.420412",
+    lng: "-66.671979",
+    zip: "00616"
+  }
+];
+
+export default REVIEWS;
