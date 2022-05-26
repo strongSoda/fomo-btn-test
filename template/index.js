fe86f9eeb6ca2e86c15676b18741e82f42ecd361
strongSoda <strongsoda2@gmail.com>
2022-05-26 17:49:33 +0530

    location based reviews

--- template/index.js
+++ template/index.js
@@ -14,18 +14,59 @@ const modalWrapper = `
 
 // const title = '<h3 class="eureka-title">Read reviews from your friends</h3>'
 // const closeBtn = '<span class="close">&times;</span>'
-// const ctaBtn = '<a id="cta" href="https://lunchbreak4kids.com/meal-plans/">Get Started</a>'
+const ctaBtn = '<a id="cta" href="https://lunchbreak4kids.com/meal-plans/">Get Started</a>'
 
-const getLocationFromIP = async () => {
-  const res = await fetch(
-    "http://api.ipstack.com/check?access_key=31d2eff2fab302c3d8c4cca4945c8faf&format=1"
-  );
-  const data = await res?.json();
-  console.log(data);
+let MY_LOCATION = null
 
-  if (data.success) return data;
-  else throw new Error(data?.error?.info);
+function distance(lat1, lat2, lon1, lon2) {
+  // The math module contains a function
+  // named toRadians which converts from
+  // degrees to radians.
+  lon1 =  lon1 * Math.PI / 180;
+  lon2 = lon2 * Math.PI / 180;
+  lat1 = lat1 * Math.PI / 180;
+  lat2 = lat2 * Math.PI / 180;
+
+  // Haversine formula
+  let dlon = lon2 - lon1;
+  let dlat = lat2 - lat1;
+  let a = Math.pow(Math.sin(dlat / 2), 2)
+            + Math.cos(lat1) * Math.cos(lat2)
+            * Math.pow(Math.sin(dlon / 2),2);
+          
+  let c = 2 * Math.asin(Math.sqrt(a));
+
+  // Radius of earth in kilometers. Use 3956
+  // for miles
+  let r = 6371;
+
+  // calculate the result
+  return(c * r);
+}
+
+const getLocationFromBrowser = async () => {
+  // console.log('here');
+  if (navigator.geolocation) {
+    // console.log('in navigator.geolocation');
+    navigator.geolocation.getCurrentPosition((position) => {
+      // console.log(position.coords.latitude, position.coords.longitude);
+      MY_LOCATION = position
+    });
+  } else { 
+    console.log("Geolocation is not supported by this browser.");
+  }
 };
+
+// const getLocationFromIP = async () => {
+//   const res = await fetch(
+//     "http://api.ipstack.com/check?access_key=31d2eff2fab302c3d8c4cca4945c8faf&format=1"
+//   );
+//   const data = await res?.json();
+//   // console.log(data);
+
+//   if (data.success) return data;
+//   else throw new Error(data?.error?.info);
+// };
 
 // TODO
 function getThreeClosestReviewsByZip(zip) {
@@ -37,42 +78,12 @@ function showReviews(closest_reviews) {}
 // TODO
 function showDefaultFallbackReviews(closest_reviews) {}
 
-function fetchReviews() {
-  /**
-   * pusedo code to get ip -> geolocation, find closest reviews & show popup
-   * or show fallback reviews if some error
-   */
-  // getLocationFromIP()
-  //   .then((zip) => {
-  //     console.log("zip", zip);
-  //     reviewsWrapper.appendChild(JSON.stringify(zip));
-  //     const closest_reviews = getThreeClosestReviewsByZip(zip);
-  //     showReviews(closest_reviews);
-  //   })
-  //   .catch((e) => {
-  //     console.error(e);
-  //     showDefaultFallbackReviews();
-  //   });
-
-  reviewsWrapper.innerHTML = modalWrapper;
-
-  const modal = document.getElementById("modal");
-  const closeButton = document.querySelector(".close-button");
-  const reviewsContent = document.getElementById("reviews-content");
-  const loadingText = document.querySelector(".eureka-title");
-
-  closeButton.addEventListener("click", () => {
-      modal.classList.toggle("show-modal");
-  });
-  console.log(reviewsWrapper.hasChildNodes());
-
+function buidReviewsDom(reviews) {
   // if (reviewsWrapper.hasChildNodes()) return;
   const reviews_container = document.createElement("div");
   reviews_container.classList.add("reviews_container");
 
-  const threeRandomReviews = REVIEWS.sort(() => .5 - Math.random()).slice(0,3)
-
-  threeRandomReviews.forEach((r) => {
+    reviews.forEach((r) => {
     const review = document.createElement("div");
     const review_text = document.createElement("blockquote");
     const review_text_h3 = document.createElement("h3");
@@ -106,11 +117,44 @@ function fetchReviews() {
     reviews_container.appendChild(review);
   });
 
+  return reviews_container
+}
+
+async function fetchReviews() {  
+  reviewsWrapper.innerHTML = modalWrapper;
+  const modal = document.getElementById("modal");
+  const closeButton = document.querySelector(".close-button");
+  const reviewsContent = document.getElementById("reviews-content");
+  const loadingText = document.querySelector(".eureka-title");
+
+  closeButton.addEventListener("click", () => {
+      modal.classList.toggle("show-modal");
+  });
+
+  await getLocationFromBrowser()
+
   setTimeout(() => {
+    const reviews = JSON.parse(JSON.stringify(REVIEWS));    
+    let reviews_container
+    if(MY_LOCATION) {
+      reviews.forEach(review => {
+        review['distance'] = distance(MY_LOCATION.coords.latitude, review?.lat, MY_LOCATION.coords.longitude, review?.lng)
+      })
+      
+      // console.log('reviews with distance', reviews);
+      const ascending_reviews = JSON.parse(JSON.stringify(reviews))
+      ascending_reviews.sort((a,b) => a.distance - b.distance) // b - a for reverse sort
+      // console.log('ascending', ascending_reviews); // b - a for reverse sort
+      reviews_container = buidReviewsDom(ascending_reviews.slice(0,3))
+    } else {
+      const threeRandomReviews = REVIEWS.sort(() => .5 - Math.random()).slice(0,3)
+      reviews_container = buidReviewsDom(threeRandomReviews)
+    }
+
     reviewsContent.innerHTML = ''
     reviewsContent.appendChild(reviews_container)
     loadingText.innerHTML = 'Reviews from your neighbors'
-    // reviewsContent.insertAdjacentHTML('afterend', ctaBtn)
+    reviewsContent.insertAdjacentHTML('afterend', ctaBtn)
   }, 3000)
   setTimeout(() => {
     modal.classList.toggle("show-modal");
