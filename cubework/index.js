521e4432d5a528f90296a9eeb513caa92f6f5a7a
strongSoda <strongsoda2@gmail.com>
2022-06-13 16:03:26 +0530

    change modal backdrop color from opaque to transparent

--- cubework/index.js
+++ cubework/index.js
@@ -26,6 +26,8 @@ const modalWrapper = `
             </div>
         </div>
 </div>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NPFGT67"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
 `
 
 const SHEETY_API_URL = "https://api.sheety.co/e6308a30988c2a9e6e0ead2fd194efa5/cubeworkMetrics"
@@ -522,6 +524,13 @@ async function fetchReviews() {
     gtag('js', new Date());
     gtag('config', 'G-RV4TQ6LHXG');
 })();
+
+// GTM
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NPFGT67');
 
 
 // Microsoft clarity
