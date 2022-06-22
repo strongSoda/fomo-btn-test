35e39920f42694f63ccfe6784f4f1cd85d3fb336
strongSoda <strongsoda2@gmail.com>
2022-06-22 09:36:50 +0530

    wip fixes

--- cubework/index.js
+++ cubework/index.js
@@ -123,7 +123,7 @@ const logUrekaCtaClick = async (ureka, currenturl, date, time, utcTime) => {
       time: time,
       ureka: ureka,
       utcTime: utcTime,
-      city: DEMOGRAPHICS_DATA.city ? DEMOGRAPHICS_DATA.city : 'NA',
+      ctaCity: DEMOGRAPHICS_DATA.city ? DEMOGRAPHICS_DATA.city : 'NA',
     }
   }
   console.log('###############', body, url);
