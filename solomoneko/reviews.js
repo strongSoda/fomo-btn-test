c282fd41dc80426190d803c626ab8e940e519b19
wulsin <wells.wulsin@gmail.com>
2022-06-24 07:41:13 -0700

    add solomoneko

---
+++ solomoneko/reviews.js
@@ -0,0 +1,29 @@
+const REVIEWS = [
+  {
+    id: 0,
+    author: "Phillip Apoloniusz",
+    text: "Solomon is creating Content Magic for Entrepreneurs when you don’t know where or how to start please come to him",
+    lat: "33.87449",
+    lng: "-118.37203",
+    zip: "90278",
+    city: "Redondo Beach",
+  },
+  {
+    id: 1,
+    author: "Olakunle Ojeniyi",
+    text: "Solomon is a very talented media professional",
+    lat: "33.87449",
+    lng: "-118.37203",
+    zip: "90278",
+    city: "Redondo Beach",
+  },
+  {
+    id: 2,
+    author: "Jonathan Olalokun",
+    text: "If you want to go far, be heard both locally and internationally...just entrust your contents and strategies to Solomon. He will handle it with a perfect touch",
+    lat: "33.87449",
+    lng: "-118.37203",
+    zip: "90278",
+    city: "Redondo Beach",
+  },
+];
