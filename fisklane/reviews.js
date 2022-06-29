956fc11cab271e2a6085a509344da3c09dec0c90
wulsin <wells.wulsin@gmail.com>
2022-06-28 22:11:24 -0700

    add fisklane

---
+++ fisklane/reviews.js
@@ -0,0 +1,29 @@
+const REVIEWS = [
+  {
+    id: 0,
+    author: "Samantha Dickens",
+    text: "I look forward to the block party every year!  We need to keep this tradition alive.",
+    lat: "33.87449",
+    lng: "-118.37203",
+    zip: "90278",
+    city: 'Fisk Lane',
+  },
+  {
+    id: 1,
+    author: "Alejandra Casillas",
+    text: "Whether you're a kid or a grownup, this is a great way to celebrate the Fourth!",
+    lat: "33.87449",
+    lng: "-118.37203",
+    zip: "90278",
+    city: 'Fisk Lane',
+  },
+  {
+    id: 2,
+    author: "Alex Wulsin, age 4",
+    text: "Whoa, whoa, whoaaaaaa!!!!!",
+    lat: "33.87449",
+    lng: "-118.37203",
+    zip: "90278",
+    city: 'Fisk Lane',
+  },
+];
