7fe60e6acba40b3677e5e6c003d7717bc414dfa0
wulsin <wells.wulsin@gmail.com>
2022-06-29 21:16:16 -0700

    add kampuskonnekt

---
+++ kampuskonnekt/reviews.js
@@ -0,0 +1,26 @@
+const REVIEWS = [
+  {
+    id: 0,
+    author: "Jeffery Asiedu Frimpong",
+    text: "Kampus Konnekt49 played an exceptional role in my journey to study in Germany, I was able to get a scholarship and accommodation even before arriving Germany. They guided me through the complex application process of studying in Germany.",
+    lat: "49.6881314",
+    lng: "6.6185184",
+    city: "Fh Trier-Environmental Campus, Germany",
+  },
+  {
+    id: 1,
+    author: "Owusu Ansah Kofi",
+    text: "After many rejections. I was introduced to Kampus Konnekt49, l have gotten admission at Eberswalde university of sustainable development with a wonderful course of my choice. I recommend to anyy one seeking admission in Germany, your ultimate solution is Kampus Konnekt49",
+    lat: "49.6881314",
+    lng: "6.6185184",
+    city: "Eberswalde University, Germany",
+  },
+  {
+    id: 2,
+    author: "Olakunle Ojeniyi",
+    text: "I can’t thank Kampus Konnekt49 enough for the support and opportunity in helping me come this far. With the level of support, assistance and advise I got from Kampus Konnekt49, my journey to Germany was as seamless as I never expected. Kampus Konnekt49 is helping me achieve my dreams, they will definitely do the same for you.",
+    lat: "49.6881314",
+    lng: "6.6185184",
+    city: "Bonner Verein für Gesundheit, Germany",
+  },
+];
