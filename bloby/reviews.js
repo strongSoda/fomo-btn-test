14963a6b295eb9e3bcde00f72c4ab8cf01d73670
strongSoda <strongsoda2@gmail.com>
2022-07-01 12:46:25 +0530

    bloby

---
+++ bloby/reviews.js
@@ -0,0 +1,158 @@
+const REVIEWS = [
+  {
+    id: 0,
+    author: "Aleks Bochkov",
+    text: 'Looks pretty cool! Congrats 🚀',
+    img:
+      "https://ph-avatars.imgix.net/756378/7ee2412f-13a6-46af-ac28-77dec7fde5ef?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=170&h=170&fit=crop&dpr=2",
+    lat: "37.774929",
+    lng: "-122.419418",
+    zip: "90278",
+    city: 'San Francisco',
+    source: 'PH'
+  },
+  {
+    id: 1,
+    author: "Natalie Karakina",
+    text:
+      'Nice shapes!',
+    img:
+      "https://ph-avatars.imgix.net/3389709/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=170&h=170&fit=crop&dpr=2",
+    lat: "41.013000",
+    lng: "28.974800",
+    zip: "90266",
+    city: 'Instanbul, Turkey',
+    source: 'PH'
+  },
+  {
+    id: 2,
+    author: "Ken Moo",
+    text:
+      "Simple. Free & Useful. Thanks!",
+    img:
+      "https://ph-avatars.imgix.net/3405672/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=170&h=170&fit=crop&dpr=2",
+    lat: "1.352083",
+    lng: "103.819839",
+    zip: "90254",
+    city: 'Singapore',
+    source: 'PH'
+  },
+    {
+    id: 3,
+    author: "Girdharee Saran",
+    text:
+      "Simple, easy and powerful idea. I will use it everytime I have to explain a design idea to my team :)",
+    img:
+      "https://ph-avatars.imgix.net/547616/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=170&h=170&fit=crop&dpr=2",
+    lat: "28.459497",
+    lng: "77.026634",
+    zip: "90254",
+    city: 'Gurugram, India',
+    source: 'PH'
+  },
+  {
+    id: 4,
+    author: "James Lindsey",
+    text:
+      "Cool and simple! Nice work!",
+    img:
+      "https://ph-avatars.imgix.net/3427775/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=170&h=170&fit=crop&dpr=2",
+    lat: "33.84022",
+    lng: "-118.35317",
+    zip: "90503",
+    city: 'Torrance',
+    source: 'PH'
+  },
+    {
+    id: 5,
+    author: "Ankit Sharma",
+    text:
+      "Awesome and clean...",
+    img:
+      "https://ph-avatars.imgix.net/2530835/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=170&h=170&fit=crop&dpr=2",
+    lat: "23.259933",
+    lng: "77.412613",
+    zip: "90266",
+    city: 'Bhopal, India',
+    source: 'PH'
+  },
+    {
+    id: 6,
+    author: "Vlad Korobov",
+    text:
+      "I would love to se more versatile blobs :)",
+    img:
+      "https://ph-avatars.imgix.net/2466375/d921fdbf-35b9-44f7-9c36-45cdc474e7e5?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=170&h=170&fit=crop&dpr=2",
+    lat: "38.9072",
+    lng: "77.0369",
+    zip: "90272",
+    city: 'Washington DC, US',
+    source: 'PH'
+  },
+    {
+    id: 7,
+    author: "François Lepine-Cossette",
+    text:
+      "I built several infographics and websites with similar blobs because of easy and cool design it adds. Really easy to use, simple task.",
+    img:
+      "https://ph-avatars.imgix.net/2939053/original",
+    lat: "45.5925",
+    lng: "73.3427",
+    zip: "90266",
+    city: 'Quebec, Canada',
+    source: 'PH'
+  },
+    {
+    id: 8,
+    author: "Dmitry Dubovik",
+    text:
+      "That's brilliant! Thank you bloby!",
+    img:
+      "https://ph-avatars.imgix.net/725764/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=170&h=170&fit=crop&dpr=2",
+    lat: "53.3498",
+    lng: "6.2603",
+    zip: "90254",
+    city: 'Dublin, Ireland',
+    source: 'PH'
+  },
+    {
+    id: 9,
+    author: "Nabeel Khan",
+    text:
+      "Looks cool mate (Y) ",
+    img:
+      "https://ph-avatars.imgix.net/499331/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=170&h=170&fit=crop&dpr=2",
+    lat: "33.8818",
+    lng: "150.8609",
+    zip: "90254",
+    city: 'Greater Sydney Area',
+    source: 'PH'
+  },
+    {
+    id: 10,
+    author: "Sinikiwe Phahlane",
+    text:
+      "Great product, congrats!",
+    img:
+      "https://ph-avatars.imgix.net/3764277/97d7216c-735b-4dda-b89a-8cf6c9ce488e?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=170&h=170&fit=crop&dpr=2",
+    lat: "26.2041",
+    lng: "28.0473",
+    zip: "90254",
+    city: 'Johannesburg, South Africa',
+    source: 'PH'
+  },
+      {
+    id: 11,
+    author: "Arpit Mathur",
+    text:
+      "Nicely done.",
+    img:
+      "https://https://ph-avatars.imgix.net/319349/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=170&h=170&fit=crop&dpr=2",
+    lat: "39.9526",
+    lng: "75.1652",
+    zip: "90254",
+    city: 'Philadelphia, United States',
+    source: 'PH'
+  },
+
+];
