a02a0dcf8c3780104090f898a62d2383824704da
wulsin <wells.wulsin@gmail.com>
2022-07-06 08:15:01 -0700

    add realhomes3d

---
+++ realhomes3d/reviews.js
@@ -0,0 +1,29 @@
+const REVIEWS = [
+  {
+    id: 0,
+    author: "John Wilkins",
+    text: "The turn around for the photos were very fast  and top quality, I got the photos the next day completely edited and ready for marketing.",
+    lat: "33.87449",
+    lng: "-118.37203",
+    zip: "90278",
+    city: "Los Angeles",
+  },
+  {
+    id: 1,
+    author: "Orville S. Henley",
+    text: "3D virtual tours are the future of real estate shopping!! I get to relax at home and view the property as if I’m actually walking inside of the home itself, this feature is astounding.",
+    lat: "33.87449",
+    lng: "-118.37203",
+    zip: "90278",
+    city: "Los Angeles",
+  },
+  {
+    id: 2,
+    author: "Elizabeth Snyder",
+    text: "Very proficient and reliable drone work I loved the bird eye view of the property it gives a different perspective of the area surrounding my home.",
+    lat: "33.87449",
+    lng: "-118.37203",
+    zip: "90278",
+    city: "Los Angeles",
+  },
+];
