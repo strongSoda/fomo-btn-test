931e489b76f6e493587f9e1d1115f3d3854d18bf
strongSoda <strongsoda2@gmail.com>
2022-07-11 14:37:04 +0530

    create carousel wall template

---
+++ template_carousel_wall/reviews.js
@@ -0,0 +1,146 @@
+const REVIEWS = [
+  {
+    id: 0,
+    author: "Lara Smith",
+    text: "I can't say enough good things about Angie and IM720.  They gave me the confidence to finally launch my business and helped ensure we were profitable from day one.",
+    img:
+      "https://cdn-cmhkh.nitrocdn.com/TezMmPgFmiYDBaHMYdPwMCWAKAYkIeiI/assets/static/optimized/www.theloremipsumco.com/wp-content/uploads/2014/04/73e4a3fe6ad959478d8562505bf1e43e.pic7_3000x2003-e1482875351271.jpg",
+    lat: "33.87449",
+    lng: "-118.37203",
+    zip: "90278",
+    city: 'Redondo Beach',
+  },
+  {
+    id: 1,
+    author: "Seth Johnson",
+    text:
+      "Angie truly is a force of nature.  So much energy, so much enthusiasm.  She has given my business the boost we needed to get off the ground.  Thank you!",
+    img:
+      "https://cdn-cmhkh.nitrocdn.com/TezMmPgFmiYDBaHMYdPwMCWAKAYkIeiI/assets/static/optimized/www.theloremipsumco.com/wp-content/uploads/2014/04/16cf23915a4dc235a62f204704f4b424.Gordon-Kelsi.jpg",
+    lat: "33.87449",
+    lng: "-118.37203",
+    zip: "90278",
+    city: 'Redondo Beach',
+  },
+  {
+    id: 2,
+    author: "Anna Williams",
+    text:
+      "Thank you IM720 for helping build my brand.  I had no idea what my brand was going to be but you helped me think through how my brand would be unique to me.  Now my customers tell me how much they love the new design of my website.",
+    img:
+      "https://cdn-cmhkh.nitrocdn.com/TezMmPgFmiYDBaHMYdPwMCWAKAYkIeiI/assets/static/optimized/www.theloremipsumco.com/wp-content/uploads/2017/01/5f92099ec3f04b259e1a4a80f5b35d6b.KellySwedaPhotography.Seth_Gewirtz_high_res-7628-e1483741109998.jpg",
+    lat: "33.87449",
+    lng: "-118.37203",
+    zip: "90278",
+    city: 'Redondo Beach',
+  },
+    {
+    id: 3,
+    author: "Douglas Brown",
+    text:
+      "IM720 knows how to deliver results.  They are true professionals who bring a wealth of experience to the table.  I wouldn't trust my brand with anyone else.",
+    img:
+      "https://cdn-cmhkh.nitrocdn.com/TezMmPgFmiYDBaHMYdPwMCWAKAYkIeiI/assets/static/optimized/www.theloremipsumco.com/wp-content/uploads/2017/01/5f92099ec3f04b259e1a4a80f5b35d6b.KellySwedaPhotography.Seth_Gewirtz_high_res-7628-e1483741109998.jpg",
+    lat: "33.87449",
+    lng: "-118.37203",
+    zip: "90278",
+    city: 'Redondo Beach',
+  },
+  {
+    id: 4,
+    author: "Luke Jones",
+    text:
+      "My website was a mess when I called up Angie, but she helped me think through a total site overhaul.  Now it looks great, is easy to use - and is generating sales at twice the monthly rate as before.  Thank you thank you thank you!",
+    img:
+      "https://cdn-cmhkh.nitrocdn.com/TezMmPgFmiYDBaHMYdPwMCWAKAYkIeiI/assets/static/optimized/www.theloremipsumco.com/wp-content/uploads/2017/01/5f92099ec3f04b259e1a4a80f5b35d6b.KellySwedaPhotography.Seth_Gewirtz_high_res-7628-e1483741109998.jpg",
+    lat: "33.88949",
+    lng: "-118.4009",
+    zip: "90266",
+    city: 'Manhattan Beach',
+  },
+    {
+    id: 5,
+    author: "Angela Garcia",
+    text:
+      "Angie's business coaching service was exactly what I needed to get my business going.  Before working with her I had a hundred ideas but no clear direction.  She helped me identify my core competencies and focus on a target customer base.  Now I have real revenue and am growing each month.  Thank you IM720!",
+    img:
+      "https://cdn-cmhkh.nitrocdn.com/TezMmPgFmiYDBaHMYdPwMCWAKAYkIeiI/assets/static/optimized/www.theloremipsumco.com/wp-content/uploads/2017/01/5f92099ec3f04b259e1a4a80f5b35d6b.KellySwedaPhotography.Seth_Gewirtz_high_res-7628-e1483741109998.jpg",
+    lat: "33.88949",
+    lng: "-118.4009",
+    zip: "90266",
+    city: 'Manhattan Beach',
+  },
+    {
+    id: 6,
+    author: "Sterling Miller",
+    text:
+      "IM720 is the best business coaching service in the South Bay, hands down.  I would not trust my business with anyone else.",
+    img:
+      "https://cdn-cmhkh.nitrocdn.com/TezMmPgFmiYDBaHMYdPwMCWAKAYkIeiI/assets/static/optimized/www.theloremipsumco.com/wp-content/uploads/2017/01/5f92099ec3f04b259e1a4a80f5b35d6b.KellySwedaPhotography.Seth_Gewirtz_high_res-7628-e1483741109998.jpg",
+    lat: "33.88949",
+    lng: "-118.4009",
+    zip: "90266",
+    city: 'Manhattan Beach',
+  },
+    {
+    id: 7,
+    author: "Mary Davis",
+    text:
+      "So many good ideas, so many helpful connections.  Angie has been an angel for our business.  Thank you so much!!",
+    img:
+      "https://cdn-cmhkh.nitrocdn.com/TezMmPgFmiYDBaHMYdPwMCWAKAYkIeiI/assets/static/optimized/www.theloremipsumco.com/wp-content/uploads/2017/01/5f92099ec3f04b259e1a4a80f5b35d6b.KellySwedaPhotography.Seth_Gewirtz_high_res-7628-e1483741109998.jpg",
+    lat: "33.88949",
+    lng: "-118.4009",
+    zip: "90266",
+    city: 'Manhattan Beach',
+  },
+    {
+    id: 8,
+    author: "Sophia Rodriguez",
+    text:
+      "I am scared of speaking with the press, so I knew I needed help.  I'm so glad I found IM720.  They helped me build a PR strategy that plays to my strengths and gets the results I need.  I am no longer afraid of getting publicity.",
+    img:
+      "https://cdn-cmhkh.nitrocdn.com/TezMmPgFmiYDBaHMYdPwMCWAKAYkIeiI/assets/static/optimized/www.theloremipsumco.com/wp-content/uploads/2017/01/5f92099ec3f04b259e1a4a80f5b35d6b.KellySwedaPhotography.Seth_Gewirtz_high_res-7628-e1483741109998.jpg",
+    lat: "33.86479",
+    lng: "-118.39719",
+    zip: "90254",
+    city: 'Hermosa Beach',
+  },
+    {
+    id: 9,
+    author: "Wally Martinez",
+    text:
+      "I would recommend IM720 to any business looking for more exposure in the media.  They are the best of the best!",
+    img:
+      "https://cdn-cmhkh.nitrocdn.com/TezMmPgFmiYDBaHMYdPwMCWAKAYkIeiI/assets/static/optimized/www.theloremipsumco.com/wp-content/uploads/2017/01/5f92099ec3f04b259e1a4a80f5b35d6b.KellySwedaPhotography.Seth_Gewirtz_high_res-7628-e1483741109998.jpg",
+    lat: "33.86479",
+    lng: "-118.39719",
+    zip: "90254",
+    city: 'Hermosa Beach',
+  },
+    {
+    id: 10,
+    author: "Louisa Hernandez",
+    text:
+      "Angie has a fantastic eye for design.  We needed a new logo but weren't happy with any of the options we had.  Angie helped us design a brand new logo that reflects our specific persona.  We love it.",
+    img:
+      "",
+    lat: "33.86479",
+    lng: "-118.39719",
+    zip: "90254",
+    city: "Hermosa Beach",
+  },
+    {
+    id: 11,
+    author: "Alex Wilson",
+    text:
+      "IM720 is a true gem - they will bend over backwards to meet your every need.  I feel so fortunate to have found them.  Give them a try - you won't regret it!",
+    img:
+      "",
+    lat: "33.86479",
+    lng: "-118.39719",
+    zip: "90254",
+    city: "Hermosa Beach",
+  },
+];
+
