d81746b1822a6d0868a92625fcbad766e958532f
strongSoda <strongsoda2@gmail.com>
2022-07-15 14:53:42 +0530

    update cities

--- ureka_carousel_wall/reviews.js
+++ ureka_carousel_wall/reviews.js
@@ -5,10 +5,10 @@ const REVIEWS = [
     text: "I can't say enough good things about Frieza and ABC.  They gave me the confidence to finally launch my business and helped ensure we were profitable from day one.",
     img:
       "https://i.pravatar.cc/",
-    lat: "33.87449",
-    lng: "-118.37203",
     zip: "90278",
-    city: 'Redondo Beach',
+    city: 'New York',
+    lat: 40.730610,
+    lng: -73.935242,
     source: 'GOOGLE'
   },
   {
@@ -18,10 +18,9 @@ const REVIEWS = [
       "Frieza truly is a force of nature.  So much energy, so much enthusiasm.  She has given my business the boost we needed to get off the ground.  Thank you!",
     img:
       "https://i.pravatar.cc/",
-    lat: "33.87449",
-    lng: "-118.37203",
-    zip: "90278",
-    city: 'Redondo Beach',
+    city: 'Delhi',
+    lat: 28.7041,
+    lng: 77.1025,
     source: 'GOOGLE'
   },
   {
@@ -31,10 +30,9 @@ const REVIEWS = [
       "Thank you ABC for helping build my brand.  I had no idea what my brand was going to be but you helped me think through how my brand would be unique to me.  Now my customers tell me how much they love the new design of my website.",
     img:
       "https://i.pravatar.cc/",
-    lat: "33.87449",
-    lng: "-118.37203",
-    zip: "90278",
-    city: 'Redondo Beach',
+    city: 'Mumbai',
+    lat: 19.0760,
+    lng: 72.8777,
     source: 'GOOGLE'
   },
     {
@@ -44,10 +42,9 @@ const REVIEWS = [
       "ABC knows how to deliver results.  They are true professionals who bring a wealth of experience to the table.  I wouldn't trust my brand with anyone else.",
     img:
       "https://i.pravatar.cc/",
-    lat: "33.87449",
-    lng: "-118.37203",
-    zip: "90278",
-    city: 'Redondo Beach',
+    city: 'Dubai',
+    lat: 25.2048,
+    lng: 55.2708,
     source: 'GOOGLE'
   },
   {
@@ -57,10 +54,9 @@ const REVIEWS = [
       "My website was a mess when I called up Frieza, but she helped me think through a total site overhaul.  Now it looks great, is easy to use - and is generating sales at twice the monthly rate as before.  Thank you thank you thank you!",
     img:
       "https://i.pravatar.cc/",
-    lat: "33.88949",
-    lng: "-118.4009",
-    zip: "90266",
-    city: 'Manhattan Beach',
+    city: 'London',
+    lat: 51.5,
+    lng: -0.1,
     source: 'GOOGLE'
   },
     {
@@ -70,10 +66,9 @@ const REVIEWS = [
       "Frieza's business coaching service was exactly what I needed to get my business going.  Before working with her I had a hundred ideas but no clear direction.  She helped me identify my core competencies and focus on a target customer base.  Now I have real revenue and am growing each month.  Thank you ABC!",
     img:
       "https://i.pravatar.cc/",
-    lat: "33.88949",
-    lng: "-118.4009",
-    zip: "90266",
-    city: 'Manhattan Beach',
+    city: 'Paris',
+    lat: 48.8566,
+    lng: 2.3522,
     source: 'GOOGLE'
   },
     {
@@ -83,10 +78,9 @@ const REVIEWS = [
       "ABC is the best business coaching service in the South Bay, hands down.  I would not trust my business with anyone else.",
     img:
       "https://i.pravatar.cc/",
-    lat: "33.88949",
-    lng: "-118.4009",
-    zip: "90266",
-    city: 'Manhattan Beach',
+    city: 'Quebec',
+    lat: 46.8138,
+    lng: -71.2080,
     source: 'GOOGLE'
   },
     {
@@ -96,10 +90,9 @@ const REVIEWS = [
       "So many good ideas, so many helpful connections.  Frieza has been an angel for our business.  Thank you so much!!",
     img:
       "https://i.pravatar.cc/",
-    lat: "33.88949",
-    lng: "-118.4009",
-    zip: "90266",
-    city: 'Manhattan Beach',
+    city: 'Amsterdam',
+    lat: 52.3738,
+    lng: 4.8910,
     source: 'GOOGLE'
   },
     {
@@ -109,10 +102,9 @@ const REVIEWS = [
       "I am scared of speaking with the press, so I knew I needed help.  I'm so glad I found ABC.  They helped me build a PR strategy that plays to my strengths and gets the results I need.  I am no longer afraid of getting publicity.",
     img:
       "https://i.pravatar.cc/",
-    lat: "33.86479",
-    lng: "-118.39719",
-    zip: "90254",
-    city: 'Hermosa Beach',
+    city: 'Sydney',
+    lat: -33.8688,
+    lng: 151.2093,
     source: 'GOOGLE'
   },
     {
@@ -122,10 +114,9 @@ const REVIEWS = [
       "I would recommend ABC to any business looking for more exposure in the media.  They are the best of the best!",
     img:
       "https://i.pravatar.cc/",
-    lat: "33.86479",
-    lng: "-118.39719",
-    zip: "90254",
-    city: 'Hermosa Beach',
+    city: 'Tokyo',
+    lat: 35.6762,
+    lng: 139.6503,
     source: 'GOOGLE'
   },
     {
@@ -135,10 +126,9 @@ const REVIEWS = [
       "Frieza has a fantastic eye for design.  We needed a new logo but weren't happy with any of the options we had.  Frieza helped us design a brand new logo that reflects our specific persona.  We love it.",
     img:
       "https://i.pravatar.cc/",
-    lat: "33.86479",
-    lng: "-118.39719",
-    zip: "90254",
-    city: "Hermosa Beach",
+    lat: "31.0461",
+    lng: "34.8516",
+    city: "Israel",
     source: 'GOOGLE'
   },
     {
@@ -148,10 +138,9 @@ const REVIEWS = [
       "ABC is a true gem - they will bend over backwards to meet your every need.  I feel so fortunate to have found them.  Give them a try - you won't regret it!",
     img:
       "https://i.pravatar.cc/",
-    lat: "33.86479",
-    lng: "-118.39719",
-    zip: "90254",
-    city: "Hermosa Beach",
+    lat: "38.7223",
+    lng: "9.1393",
+    city: "Lisbon",
     source: 'GOOGLE'
   },
 ];
