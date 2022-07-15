67f577c5e2cd792128a356e99366a266d7b880b0
strongSoda <strongsoda2@gmail.com>
2022-07-15 12:24:01 +0530

    demo carousel wall for ureka website

---
+++ ureka_carousel_wall/reviews.js
@@ -0,0 +1,158 @@
+const REVIEWS = [
+  {
+    id: 0,
+    author: "Lara Smith",
+    text: "I can't say enough good things about Frieza and ABC.  They gave me the confidence to finally launch my business and helped ensure we were profitable from day one.",
+    img:
+      "https://i.pravatar.cc/",
+    lat: "33.87449",
+    lng: "-118.37203",
+    zip: "90278",
+    city: 'Redondo Beach',
+    source: 'GOOGLE'
+  },
+  {
+    id: 1,
+    author: "Seth Johnson",
+    text:
+      "Frieza truly is a force of nature.  So much energy, so much enthusiasm.  She has given my business the boost we needed to get off the ground.  Thank you!",
+    img:
+      "https://i.pravatar.cc/",
+    lat: "33.87449",
+    lng: "-118.37203",
+    zip: "90278",
+    city: 'Redondo Beach',
+    source: 'GOOGLE'
+  },
+  {
+    id: 2,
+    author: "Anna Williams",
+    text:
+      "Thank you ABC for helping build my brand.  I had no idea what my brand was going to be but you helped me think through how my brand would be unique to me.  Now my customers tell me how much they love the new design of my website.",
+    img:
+      "https://i.pravatar.cc/",
+    lat: "33.87449",
+    lng: "-118.37203",
+    zip: "90278",
+    city: 'Redondo Beach',
+    source: 'GOOGLE'
+  },
+    {
+    id: 3,
+    author: "Douglas Brown",
+    text:
+      "ABC knows how to deliver results.  They are true professionals who bring a wealth of experience to the table.  I wouldn't trust my brand with anyone else.",
+    img:
+      "https://i.pravatar.cc/",
+    lat: "33.87449",
+    lng: "-118.37203",
+    zip: "90278",
+    city: 'Redondo Beach',
+    source: 'GOOGLE'
+  },
+  {
+    id: 4,
+    author: "Luke Jones",
+    text:
+      "My website was a mess when I called up Frieza, but she helped me think through a total site overhaul.  Now it looks great, is easy to use - and is generating sales at twice the monthly rate as before.  Thank you thank you thank you!",
+    img:
+      "https://i.pravatar.cc/",
+    lat: "33.88949",
+    lng: "-118.4009",
+    zip: "90266",
+    city: 'Manhattan Beach',
+    source: 'GOOGLE'
+  },
+    {
+    id: 5,
+    author: "Angela Garcia",
+    text:
+      "Frieza's business coaching service was exactly what I needed to get my business going.  Before working with her I had a hundred ideas but no clear direction.  She helped me identify my core competencies and focus on a target customer base.  Now I have real revenue and am growing each month.  Thank you ABC!",
+    img:
+      "https://i.pravatar.cc/",
+    lat: "33.88949",
+    lng: "-118.4009",
+    zip: "90266",
+    city: 'Manhattan Beach',
+    source: 'GOOGLE'
+  },
+    {
+    id: 6,
+    author: "Sterling Miller",
+    text:
+      "ABC is the best business coaching service in the South Bay, hands down.  I would not trust my business with anyone else.",
+    img:
+      "https://i.pravatar.cc/",
+    lat: "33.88949",
+    lng: "-118.4009",
+    zip: "90266",
+    city: 'Manhattan Beach',
+    source: 'GOOGLE'
+  },
+    {
+    id: 7,
+    author: "Mary Davis",
+    text:
+      "So many good ideas, so many helpful connections.  Frieza has been an angel for our business.  Thank you so much!!",
+    img:
+      "https://i.pravatar.cc/",
+    lat: "33.88949",
+    lng: "-118.4009",
+    zip: "90266",
+    city: 'Manhattan Beach',
+    source: 'GOOGLE'
+  },
+    {
+    id: 8,
+    author: "Sophia Rodriguez",
+    text:
+      "I am scared of speaking with the press, so I knew I needed help.  I'm so glad I found ABC.  They helped me build a PR strategy that plays to my strengths and gets the results I need.  I am no longer afraid of getting publicity.",
+    img:
+      "https://i.pravatar.cc/",
+    lat: "33.86479",
+    lng: "-118.39719",
+    zip: "90254",
+    city: 'Hermosa Beach',
+    source: 'GOOGLE'
+  },
+    {
+    id: 9,
+    author: "Wally Martinez",
+    text:
+      "I would recommend ABC to any business looking for more exposure in the media.  They are the best of the best!",
+    img:
+      "https://i.pravatar.cc/",
+    lat: "33.86479",
+    lng: "-118.39719",
+    zip: "90254",
+    city: 'Hermosa Beach',
+    source: 'GOOGLE'
+  },
+    {
+    id: 10,
+    author: "Louisa Hernandez",
+    text:
+      "Frieza has a fantastic eye for design.  We needed a new logo but weren't happy with any of the options we had.  Frieza helped us design a brand new logo that reflects our specific persona.  We love it.",
+    img:
+      "https://i.pravatar.cc/",
+    lat: "33.86479",
+    lng: "-118.39719",
+    zip: "90254",
+    city: "Hermosa Beach",
+    source: 'GOOGLE'
+  },
+    {
+    id: 11,
+    author: "Alex Wilson",
+    text:
+      "ABC is a true gem - they will bend over backwards to meet your every need.  I feel so fortunate to have found them.  Give them a try - you won't regret it!",
+    img:
+      "https://i.pravatar.cc/",
+    lat: "33.86479",
+    lng: "-118.39719",
+    zip: "90254",
+    city: "Hermosa Beach",
+    source: 'GOOGLE'
+  },
+];
+
