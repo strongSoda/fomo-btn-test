076ba702e085b348c570244e7761cc4c02e3dd78
strongSoda <strongsoda2@gmail.com>
2022-07-24 18:18:37 +0530

    updates images

--- ureka_carousel_wall/reviews.js
+++ ureka_carousel_wall/reviews.js
@@ -4,7 +4,7 @@ const REVIEWS = [
     author: "Lara Smith",
     text: "I can't say enough good things about Frieza and ABC.  They gave me the confidence to finally launch my business and helped ensure we were profitable from day one.",
     img:
-      "https://imgur.com/a/QppXhNY",
+      "https://i.imgur.com/CZ85c1L.jpg",
     zip: "90278",
     city: 'New York',
     lat: 40.730610,
@@ -17,7 +17,7 @@ const REVIEWS = [
     text:
       "Frieza truly is a force of nature.  So much energy, so much enthusiasm.  She has given my business the boost we needed to get off the ground.  Thank you!",
     img:
-      "https://imgur.com/2QjzcYZ",
+      "https://i.imgur.com/2QjzcYZ.jpg",
     city: 'Delhi',
     lat: 28.7041,
     lng: 77.1025,
@@ -29,7 +29,7 @@ const REVIEWS = [
     text:
       "Thank you ABC for helping build my brand.  I had no idea what my brand was going to be but you helped me think through how my brand would be unique to me.  Now my customers tell me how much they love the new design of my website.",
     img:
-      "https://imgur.com/FDxNzvF",
+      "https://i.imgur.com/FDxNzvF.jpg",
     city: 'Mumbai',
     lat: 19.0760,
     lng: 72.8777,
@@ -41,7 +41,7 @@ const REVIEWS = [
     text:
       "ABC knows how to deliver results.  They are true professionals who bring a wealth of experience to the table.  I wouldn't trust my brand with anyone else.",
     img:
-      "https://imgur.com/MGSXw8G",
+      "https://i.imgur.com/MGSXw8G.jpg",
     city: 'Dubai',
     lat: 25.2048,
     lng: 55.2708,
@@ -53,7 +53,7 @@ const REVIEWS = [
     text:
       "My website was a mess when I called up Frieza, but she helped me think through a total site overhaul.  Now it looks great, is easy to use - and is generating sales at twice the monthly rate as before.  Thank you thank you thank you!",
     img:
-      "https://imgur.com/4sXaHYf",
+      "https://i.imgur.com/4sXaHYf.jpg",
     city: 'London',
     lat: 51.5,
     lng: -0.1,
@@ -65,7 +65,7 @@ const REVIEWS = [
     text:
       "Frieza's business coaching service was exactly what I needed to get my business going.  Before working with her I had a hundred ideas but no clear direction.  She helped me identify my core competencies and focus on a target customer base.  Now I have real revenue and am growing each month.  Thank you ABC!",
     img:
-      "https://imgur.com/xE7K5XU",
+      "https://i.imgur.com/xE7K5XU.jpg",
     city: 'Paris',
     lat: 48.8566,
     lng: 2.3522,
@@ -77,7 +77,7 @@ const REVIEWS = [
     text:
       "ABC is the best business coaching service in the South Bay, hands down.  I would not trust my business with anyone else.",
     img:
-      "https://imgur.com/x79WZO1",
+      "https://i.imgur.com/x79WZO1.jpg",
     city: 'Quebec',
     lat: 46.8138,
     lng: -71.2080,
@@ -89,7 +89,7 @@ const REVIEWS = [
     text:
       "So many good ideas, so many helpful connections.  Frieza has been an angel for our business.  Thank you so much!!",
     img:
-      "https://imgur.com/BxpHSHS",
+      "https://i.imgur.com/BxpHSHS.jpg",
     city: 'Amsterdam',
     lat: 52.3738,
     lng: 4.8910,
@@ -101,7 +101,7 @@ const REVIEWS = [
     text:
       "I am scared of speaking with the press, so I knew I needed help.  I'm so glad I found ABC.  They helped me build a PR strategy that plays to my strengths and gets the results I need.  I am no longer afraid of getting publicity.",
     img:
-      "https://imgur.com/xH50XNR",
+      "https://i.imgur.com/xH50XNR.jpg",
     city: 'Sydney',
     lat: -33.8688,
     lng: 151.2093,
@@ -113,7 +113,7 @@ const REVIEWS = [
     text:
       "I would recommend ABC to any business looking for more exposure in the media.  They are the best of the best!",
     img:
-      "https://imgur.com/MGIzDsa",
+      "https://i.imgur.com/MGIzDsa.jpg",
     city: 'Tokyo',
     lat: 35.6762,
     lng: 139.6503,
@@ -125,7 +125,7 @@ const REVIEWS = [
     text:
       "Frieza has a fantastic eye for design.  We needed a new logo but weren't happy with any of the options we had.  Frieza helped us design a brand new logo that reflects our specific persona.  We love it.",
     img:
-      "https://imgur.com/dPQDFgD",
+      "https://i.imgur.com/dPQDFgD.jpg",
     lat: "31.0461",
     lng: "34.8516",
     city: "Israel",
@@ -137,7 +137,7 @@ const REVIEWS = [
     text:
       "ABC is a true gem - they will bend over backwards to meet your every need.  I feel so fortunate to have found them.  Give them a try - you won't regret it!",
     img:
-      "https://imgur.com/CZ85c1L",
+      "https://i.imgur.com/CZ85c1L.jpg",
     lat: "38.7223",
     lng: "9.1393",
     city: "Lisbon",
